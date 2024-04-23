3b781cbf620f8f65e739080bcf639a44503770c1
Gia Phong <ngphong.7800@gmail.com>
2024-04-23 22:23:45 +0700

    update v2

---
+++ lib/screen/loginpage.dart
@@ -0,0 +1,235 @@
+import 'package:demo_flutter/screen/home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class MyLoginPage extends StatefulWidget {
+  @override
+  _MyLoginPageSate createState() => _MyLoginPageSate();
+}
+
+class _MyLoginPageSate extends State<MyLoginPage> {
+  late String email = "admin@admin.com";
+  late String password = "123123";
+  late String deEmail = "";
+  late String dePass = "";
+  void _login() {
+    if (email == deEmail && password == dePass) {
+      MaterialPageRoute(builder: (context) => MyHomePage());
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Login Failed'),
+          content: Text('Incorrect email or password.'),
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
+
+@override
+Widget build(BuildContext context) {
+  return SingleChildScrollView(
+    child: Stack(
+      alignment: Alignment.center,
+      children: [
+        Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 50),
+              child: Container(
+                color: Colors.white,
+                child: Center(
+                  child: Image.asset(
+                    'assets/logoapp.jpg',
+                    width: 50,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Container(
+                color: Colors.white,
+                child: Center(
+                  child: Text(
+                    'TaskMaster',
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Container(
+                color: Colors.white,
+                child: Center(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        value = deEmail;
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 10, left: 20, right: 20),
+              child: Container(
+                color: Colors.white,
+                child: Center(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'PassWord',
+                      border: OutlineInputBorder(),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        dePass = value;
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 20),
+              child: Container(
+                color: Colors.white,
+                child: GestureDetector(
+                    onTap: () {
+                      // viet navigate vô đây
+                    },
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        'Forgot Password?',
+                        textAlign: TextAlign.right,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    )),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: Container(
+                color: Colors.white,
+                child: Center(
+                  child: TextButton(
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.black),
+                      padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                        EdgeInsets.symmetric(vertical: 12, horizontal: 165),
+                      ),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                    ),
+                    onPressed: () {
+                      _login();
+                    },
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                          fontSize: 18),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: Container(
+                color: Colors.white,
+                child: Center(
+                  child: Text(
+                    'Or',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Container(
+                color: Colors.white,
+                child: Center(
+                  child: TextButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          Color.fromARGB(100, 219, 219, 219)),
+                      padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                        EdgeInsets.symmetric(vertical: 6, horizontal: 10),
+                      ),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Image.asset(
+                          'assets/logoGG.gif',
+                          height: 40,
+                        ),
+                        SizedBox(width: 60),
+                        Text(
+                          'Continue with Google',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 0, 0, 0),
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: Container(
+                color: Colors.white,
+                child: Center(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text("Don't have an account?"),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: Container(
+                color: Colors.white,
+                child: Center(
+                  child: Text(
+                    "SignUp",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )
+      ],
+    ),
+  );
+}
